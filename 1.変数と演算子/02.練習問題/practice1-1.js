32a01b06d9c22c1974f10bafd6e776c5d0f8d0ab
cs <xxxx@example.com>
2022-02-24 11:49:42 +0900

    feat:問3までの解答

--- 1.変数と演算子/02.練習問題/practice1-1.js
+++ 1.変数と演算子/02.練習問題/practice1-1.js
@@ -18,30 +18,31 @@ console.log(b);
 //     修正後の変数名は自由に決めてください。
 
 
-const triangleArea;
-const let = 3;
-const height = 2;
-triangleArea = let * height / 2;
+// const triangleArea;
+const num01 = 3;
+const num02 = 2;
+const triangleArea = num01 * num02 / 2;
 console.log(triangleArea);
 
 
 // 問2.以下のコードには無駄な記述があります。効率のよいコードに書き換えてください。
 
-let circleArea;
+// let circleArea;
 let num1 = 5;
-let num2 = 5;
+// let num2 = 5;
 
-circleArea = num1 * num2 * 3.14;
+let circleArea = num1 * num1 * 3.14;
 console.log(circleArea);
 
 
 // 問3.円錐の面積(coneVolume)を計算してください。
 // 　　計算式は円の面積(BottomArea) × 高さ(height) ÷ 3です。
 
-let coneVolume;
+// let coneVolume;
 let BottomArea = 100;
 let height = 50;
 
+let coneVolume = BottomArea * height / 3;
 
 console.log(coneVolume);
 
