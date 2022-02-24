c413fb7b080ee8cd208e00c3134b9b85bf74b9aa
cs <xxxx@example.com>
2022-02-24 12:15:26 +0900

    feat:練習問題2の解答

--- 1.変数と演算子/02.練習問題/practice1-2.js
+++ 1.変数と演算子/02.練習問題/practice1-2.js
@@ -6,18 +6,20 @@
     const itemName2 = "りんご";
     const itemPrice2 = 120;
     const TAX = 1.1;
-    const msg = itemName + "と" + itemName2 + "の合計は" + itemPrice + itemPrice2 * TAX + "円(税込)です。 税抜(" + itemPrice + itemPrice2 + "円)"
+    const msg = itemName + "と" + itemName2 + "の合計は" + ((itemPrice + itemPrice2) * TAX) + "円(税込)です。 税抜(" + (itemPrice + itemPrice2) + "円)"
     console.log(msg);
 
 
 // 問2.問1のコードをテンプレートリテラルで記述しなおしてください。
 
-    const itemName = "みかん";
-    const itemPrice = 170;
-    const itemName2 = "りんご";
-    const itemPrice2 = 120;
-    const TAX = 1.1;
+    // const itemName = "みかん";
+    // const itemPrice = 170;
+    // const itemName2 = "りんご";
+    // const itemPrice2 = 120;
+    // const TAX = 1.1;
 
+    const msg2 = `${itemName}と${itemName2}の合計は${(itemPrice + itemPrice2) * TAX}円(税込)です。 税抜(${itemPrice + itemPrice2}円)`
+    console.log(msg2);
 
 
 // 問3. document.body.innerHTMLに値を代入するとhtmlのbody要素の中に値を挿入することができます。
@@ -25,7 +27,7 @@
 //          また、ベタ打ちするのではなく変数traineeNameを必ず使用してください。
 
     const traineeName = "佐藤";
-    document.body.innerHTML = `test`;
+    document.body.innerHTML = `<h1>${traineeName}さん、こんにちは！</h1>`;
 
 
 // 問4(★). 二次方程式 x^2 -5x + 4 = 0　の解を計算し、コンソールへ表示するコードを記述してください。
